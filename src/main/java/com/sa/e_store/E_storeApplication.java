e5545e5f69bccef60a093c156ea00f114e08d66a
Your Name <houbbadi.abdelghani@gmail.com>
2022-09-13 23:08:12 -0500

    starting the e_store business project

---
+++ src/main/java/com/sa/e_store/E_storeApplication.java
@@ -0,0 +1,11 @@
+package com.sa.e_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E_storeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(E_storeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/sa/e_store/E_storeApplicationTests.java
@@ -0,0 +1,10 @@
+package com.sa.e_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class E_storeApplicationTests {
+    @Test
+    void contextLoads() {
+    }}
